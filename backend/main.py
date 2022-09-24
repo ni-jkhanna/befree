6f2e7723c0d0922f5b31c6c70d4c7906fe2e4900
antbaumann <antbaumann@gmail.com>
2022-09-24 19:21:30 +0200

    add cors to backend

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 
 @app.route('/getAllPosts', methods=['GET'])
 def getAllPosts():
